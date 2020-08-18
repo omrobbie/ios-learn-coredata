31351fa53aa2280c29789b168445f5a1266b05f6
omrobbie <putu.roby@yahoo.com>
2020-08-18 13:29:11 +0800

    setup layout with dummy data

--- CoreData-001/CoreData-001/ViewController.swift
+++ CoreData-001/CoreData-001/ViewController.swift
@@ -10,7 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    @IBAction func btnAddNameTapped(_ sender: Any) {
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Item \(indexPath.row)"
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
